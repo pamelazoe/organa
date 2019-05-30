338b137700dc0060ca40a651c8d0b59038a9c65e
Pamela <pmlzoe@gmail.com>
2019-05-29 19:05:38 -0500

    Se agrega código para almacenar nombre en array

--- public/qr.js
+++ public/qr.js
@@ -5,6 +5,7 @@ let loadingMessage = document.getElementById("loadingMessage");
 let outputContainer = document.getElementById("output");
 let outputMessage = document.getElementById("outputMessage");
 let outputData = document.getElementById("outputData");
+let dataName= new Array();
 
 function drawLine(begin, end, color) {
   canvas.beginPath();
@@ -16,14 +17,17 @@ function drawLine(begin, end, color) {
 }
 
 // Use facingMode: environment to attemt to get the front camera on phones
-navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } }).then(function(stream) {
+navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } })
+.then(function(stream) {
   video.srcObject = stream;
   video.setAttribute("playsinline", true); // required to tell iOS safari we don't want fullscreen
   video.play();
+
   requestAnimationFrame(tick);
-});
+})
 
 function tick() {
+
   loadingMessage.innerText = "⌛ Loading video..."
   if (video.readyState === video.HAVE_ENOUGH_DATA) {
     loadingMessage.hidden = true;
@@ -45,10 +49,20 @@ function tick() {
       outputMessage.hidden = true;
       outputData.parentElement.hidden = false;
       outputData.innerText = code.data;
+
+      if (!dataName.includes(code.data)) {
+            dataName.push(code.data);
+            console.log(dataName);
+          }
     } else {
+
       outputMessage.hidden = false;
-      outputData.parentElement.hidden = true;
+      //outputData.parentElement.hidden = true;
     }
+
   }
+  //video.stop(code.data);
+
+//console.log(dataName);
   requestAnimationFrame(tick);
 }
